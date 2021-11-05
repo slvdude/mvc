764f4f9a929402c9b03aa4645d89c5ab7cdcbada
sergey <slavdudeight@gmail.com>
2021-11-05 21:49:43 +0400

    styles were added

--- views/authForm.php
+++ views/authForm.php
@@ -1,10 +1,10 @@
 <div class="container">
-    <div class="form-container">
-        <form action="index.php?controller=AuthController&action=authUser" method="post"> 
+    <div class="formContainer">
+        <form action="index.php?controller=AuthController&action=authUser" method="post" class="authForm"> 
             <h2>Sign in</h2>
-            <input type="text" name="login" placeholder="Login"><br>
-            <input type="password" name="password" placeholder="Password"><br>
-            <button type="submit" name="AuthController">Sign in</button><br>
+            <input type="text" name="login" placeholder="Login" class="authInput"><br>
+            <input type="password" name="password" placeholder="Password" class="authInput"><br>
+            <button type="submit" name="AuthController" class="authBtn">Sign in</button><br>
         </form>
     </div>
     <p><?php echo trim(strip_tags($_GET['error']))?></p>
--- views/todoView.php
+++ views/todoView.php
@@ -1,16 +1,16 @@
-<div class="todo-container">
-    <h1 style="text-align: center;">Task list</h1>
+<div class="handleContainer">
+    <h1 class="title">Task list</h1>
     <hr>
-    <form action="index.php?controller=TodoController&action=addTodo" method="POST" class="create_task">
-        <input type="text" placeholder="Enter a task" name="title">
-        <button type="submit" name="addTodo">Add task</button>
+    <form action="index.php?controller=TodoController&action=addTodo" method="POST" class="createTask">
+        <input type="text" placeholder="Enter a task" name="title" class="inputTask">
+        <button type="submit" name="addTodo" class="addTask">Add task</button>
     </form>
-    <div>
+    <div class="handleBtns">
         <form action="index.php?controller=TodoController&action=deleteAllTodos" method="POST">
-            <button type="submit" name="deleteAll">Remove all</button>   
+            <button type="submit" name="deleteAll" class="handleTask">Remove all</button>   
         </form>
         <form action="index.php?controller=TodoController&action=doneAllTodos" method="POST">
-            <button type="submit" name="doneAll">Ready all</button>
+            <button type="submit" name="doneAll" class="handleTask">Ready all</button>
         </form> 
     </div>
     <hr>
@@ -18,10 +18,10 @@
         <ul>
             <?php foreach($todoArr as $todo): ?>
                 <li>
-                    <div class='todo_container'>
+                    <div class='todoContainer'>
                         <div>
-                            <p><?php echo $todo['description'] ?></p>
-                            <div>
+                            <p class="description"><?php echo $todo['description'] ?></p>
+                            <div class="btnContainer">
                                 <form action="index.php?controller=TodoController&action=doneTodo" method="POST">
                                     <input type="hidden" name="todo_id" value="<?php echo $todo['id']?>">
                                     <button name="doneTodo" type="submit">Ready</button>
@@ -42,7 +42,7 @@
     <?php endif; ?>
 </div>
 
-<div class="logout-container">
+<div class="logoutContainer">
     <form action="index.php?controller=AuthController&action=logout" method="post">
         <button class="logout">Logout</button>
     </form>               
